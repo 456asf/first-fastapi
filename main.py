e36163d6b75226c0b2256f8fd8862b74f7a75792
saeed.faraji <saeed.faraji1995@gmail.com>
2023-11-09 08:37:31 +0330

    Adding list of books

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI 
+from fastapi import (
+    FastAPI,Path,status
+)
 
 app = FastAPI()
+
+books = [   
+    {"title":"book-1","content":"content-1","isbn":"234234-23423-42323"},
+    {"title":"book-2","content":"content-2","isbn":"234234-23423-4233"},
+    {"title":"book-3","content":"content-3","isbn":"23234-232323-4223"},
+    {"title":"book-4","content":"content-4","isbn":"23234-234423-4233"}
+]
 
 @app.get("/")
 async def root():
     return {"message":"It's worked!"}
+
+@app.get("/books/",status_code=status.HTTP_200_OK)
+def get_books():
+    return books 
